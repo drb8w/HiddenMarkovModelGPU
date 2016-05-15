4146bd7730aab2ca385eee036311d4890320bdbd
Marc <marc.dhaubenstock@gmail.com>
2016-05-15 18:55:55 +0200

    added config files for a summer day HMM

--- CPP_HMM/CPP_HMM/src/trainhmm.cpp
+++ CPP_HMM/CPP_HMM/src/trainhmm.cpp
@@ -5,24 +5,38 @@ using namespace std;
 
 #include "hmm.h"
 
+//int main(int argc, char* argv[])
+//{
+//  Hmm hmm;
+//  if (argc<3) {
+//    cerr << "USAGE: trainhmm INIT-HMM RESULT-HMM DATA [MAX-ITERATIONS]" << endl;
+//    exit(1);
+//  }
+//
+//  hmm.loadProbs(argv[1]);
+//  const char* output = argv[2];
+//  ifstream istrm(argv[3]);
+//  int maxIterations = 10;
+//  if (argc>4)
+//    maxIterations = atoi(argv[4]);
+//
+//  vector<vector<unsigned long>*> trainingSequences;
+//  hmm.readSeqs(istrm, trainingSequences);
+//  hmm.baumWelch(trainingSequences, maxIterations);
+//  hmm.saveProbs(output);
+//}
+
 int main(int argc, char* argv[])
 {
-  Hmm hmm;
-  if (argc<3) {
-    cerr << "USAGE: trainhmm INIT-HMM RESULT-HMM DATA [MAX-ITERATIONS]" << endl;
-    exit(1);
-  }
+	Hmm hmm;
 
-  hmm.loadProbs(argv[1]);
-  const char* output = argv[2];
-  ifstream istrm(argv[3]);
-  int maxIterations = 10;
-  if (argc>4)
-    maxIterations = atoi(argv[4]);
-
-  vector<vector<unsigned long>*> trainingSequences;
-  hmm.readSeqs(istrm, trainingSequences);
-  hmm.baumWelch(trainingSequences, maxIterations);
-  hmm.saveProbs(output);
+	if (argc<2) {
+		cerr << "USAGE: genseq NAME N" << endl
+			<< "generates N observation sequences using the HMM with the given NAME" << endl;
+		exit(1);
+	}
+	hmm.loadProbs(argv[1]);
+	int seqs = atoi(argv[2]);
+	hmm.genSeqs(cout, seqs);
 }
   
