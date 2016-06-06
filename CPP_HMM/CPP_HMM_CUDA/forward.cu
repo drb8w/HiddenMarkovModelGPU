0d8dbe9c3fd7694e54ffb13183eb1dcd5f3d7d47
drb8w <drb8w@directbox.com>
2016-06-06 22:33:19 +0200

    * switched code to ROW_MAJ_ORD_MAT_ROW_FIRST_INDEX (hopefully that what is intended)

--- CPP_HMM/CPP_HMM_CUDA/forward.cu
+++ CPP_HMM/CPP_HMM_CUDA/forward.cu
@@ -330,13 +330,37 @@ __host__ cudaError_t ForwardAlgorithmGPU(const double *dev_Pi_startProbs_1D, con
 	// HACK: symbol id is same as index
 	int idx_obs_start = obs_start;
 
+#ifdef COL_MAJ_ORD_MAT_ROW_FIRST_INDEX
+	
+	int dim1_B = N_noOfStates;
+	int dim1_Alpha = T_noOfObservations;
 	// init first row of trellis
 	for (unsigned int i = 0; i < N_noOfStates; i++)
 	{
-		double alpha_0_i = dev_Pi_startProbs_1D[i] * dev_B_obsEmissionProbs_2D[i*V_noOfObsSymbols + idx_obs_start];
-		dev_Alpha_trelis_2D[i] = alpha_0_i;
+		int idx_b_i_idxOs = i + idx_obs_start * dim1_B;
+		int idx_alpha_0i = i*dim1_Alpha;
+		int idx_pi_i = i;
+
+		double alpha_0_i = dev_Pi_startProbs_1D[idx_pi_i] * dev_B_obsEmissionProbs_2D[idx_b_i_idxOs];
+		dev_Alpha_trelis_2D[idx_alpha_0i] = alpha_0_i;
 	}
 
+#endif
+
+#ifdef ROW_MAJ_ORD_MAT_ROW_FIRST_INDEX
+
+	// init first row of trellis
+	for (unsigned int i = 0; i < N_noOfStates; i++)
+	{
+		int idx_b_i_idxOs = i*V_noOfObsSymbols + idx_obs_start;
+		int idx_alpha_0i = i;
+		int idx_pi_i = i;
+
+		double alpha_0_i = dev_Pi_startProbs_1D[idx_pi_i] * dev_B_obsEmissionProbs_2D[idx_b_i_idxOs];
+		dev_Alpha_trelis_2D[idx_alpha_0i] = alpha_0_i;
+	}
+
+#endif
 	// ------------------------------------------------------------------------------------------------------
 
 	for (unsigned int idx_obs = 1; idx_obs < T_noOfObservations; idx_obs++){
