0921200bd5d766b58f4133a32c8e5a801f6f13f3
drb8w <drb8w@directbox.com>
2016-06-09 15:33:05 +0200

    * problem: used API only available at 3.5 and above...

--- CPP_HMM/CPP_HMM_CUDA/VectorMath.cu
+++ CPP_HMM/CPP_HMM_CUDA/VectorMath.cu
@@ -4,7 +4,7 @@
 // CPU - serial implementation
 //---------------------------------------------------------------------------------------------------------
 
-__host__ double rowColumnMulMatrixHost(const double *U, const double *V, int index_row_i, int index_column_j, int dim1_U, int dim1_V)
+__host__ double rowColumnMulMatrixHost(const double *host_U, const double *host_V, int index_row_i, int index_column_j, int dim1_U, int dim1_V)
 {
 	//---------------------------------------------------------------------------------------------------------
 	// determine index - row first
@@ -14,7 +14,7 @@ __host__ double rowColumnMulMatrixHost(const double *U, const double *V, int ind
 	int idx_v_0j = index_column_j;
 
 	//---------------------------------------------------------------------------------------------------------
-	// iterate the arrays
+	// iterate the arrays - row major
 	//---------------------------------------------------------------------------------------------------------
 	double result = 0;
 
@@ -23,15 +23,32 @@ __host__ double rowColumnMulMatrixHost(const double *U, const double *V, int ind
 		int idx_u_ik = idx_u_i0 + k;
 		int idx_v_kj = idx_v_0j + k*dim1_V;
 
-		result += U[idx_u_ik] * V[idx_v_kj];
+		result += host_U[idx_u_ik] * host_V[idx_v_kj];
 	}
 
 	return result;
 }
 
-__host__ void elementMulMatrixHost(double *w, const double *U, const double *V, int index_row_i, int index_column_j, int dim1_U, int dim1_V)
+__host__ void elementMulMatrixHost(double *host_w, const double *host_U, const double *host_V, int index_row_i, int index_column_j, int dim1_U, int dim1_V)
 {
+	//---------------------------------------------------------------------------------------------------------
+	// determine index - row first
+	//---------------------------------------------------------------------------------------------------------
 
+	int idx_u_i0 = index_row_i * dim1_U;
+	int idx_v_0j = index_column_j;
+
+	//---------------------------------------------------------------------------------------------------------
+	// iterate the arrays - row major
+	//---------------------------------------------------------------------------------------------------------
+	
+	for (int idx_k = 0; idx_k < dim1_U; idx_k++)
+	{
+		int idx_u_ik = idx_u_i0 + idx_k;
+		int idx_v_kj = idx_v_0j + idx_k*dim1_V;
+
+		host_w[idx_k] = host_U[idx_u_ik] * host_V[idx_v_kj];
+	}
 }
 
 //---------------------------------------------------------------------------------------------------------
@@ -39,7 +56,7 @@ __host__ void elementMulMatrixHost(double *w, const double *U, const double *V, 
 // e.g. with reduction
 //---------------------------------------------------------------------------------------------------------
 
-__device__ double rowColumnMulMatrixDevice(const double *U, const double *V, int index_row_i, int index_column_j, int dim1_U, int dim1_V)
+__device__ double rowColumnMulMatrixDevice(const double *dev_U, const double *dev_V, int index_row_i, int index_column_j, int dim1_U, int dim1_V)
 {
 	//---------------------------------------------------------------------------------------------------------
 	// determine index
@@ -50,9 +67,68 @@ __device__ double rowColumnMulMatrixDevice(const double *U, const double *V, int
 	return 0;
 }
 
-__device__ void elementMulMatrixDevice(double *w, const double *U, const double *V, int index_row_i, int index_column_j, int dim1_U, int dim1_V)
+__global__ void rowColumnMulMatrixKernel(double *dev_w, const double *dev_U, const double *dev_V, int index_row_i, int index_column_j, int dim1_U, int dim1_V)
 {
 
-	// second dimension of matrices needed to stop additional threads from executing
+	//---------------------------------------------------------------------------------------------------------
+	// determine index
+	//---------------------------------------------------------------------------------------------------------
+
+
+	//---------------------------------------------------------------------------------------------------------
+
 }
 
+__device__ void elementMulMatrixDevice(double *dev_w, const double *dev_U, const double *dev_V, int index_row_i, int index_column_j, int dim1_U, int dim1_V)
+{
+	cudaError_t cudaStatus;
+
+	// second dimension of matrices needed to stop additional threads from executing
+
+	//---------------------------------------------------------------------------------------------------------
+
+
+	// Launch a kernel on the GPU with one thread for each element.
+	//elementMulMatrixKernel << <1, dim1_U >> >(dev_w, dev_U, dev_V, index_row_i, index_column_j, dim1_U, dim1_V);
+
+
+	//---------------------------------------------------------------------------------------------------------
+	// syncronize
+	//---------------------------------------------------------------------------------------------------------
+	//cudaStatus = cudaDeviceSynchronize();
+
+	//---------------------------------------------------------------------------------------------------------
+	// do reduction
+	//---------------------------------------------------------------------------------------------------------
+
+
+
+}
+
+__global__ void elementMulMatrixKernel(double *dev_w, const double *dev_U, const double *dev_V, int index_row_i, int index_column_j, int dim1_U, int dim1_V)
+{
+	//---------------------------------------------------------------------------------------------------------
+	// determine index - row first
+	//---------------------------------------------------------------------------------------------------------
+
+	int idx_k = threadIdx.x;
+
+	// check index range to abort
+	if (idx_k > dim1_U)
+		return;
+
+	int idx_u_i0 = index_row_i * dim1_U;
+	int idx_v_0j = index_column_j;
+
+	int idx_u_ik = idx_u_i0 + idx_k;
+	int idx_v_kj = idx_v_0j + idx_k*dim1_V;
+
+	//---------------------------------------------------------------------------------------------------------
+	// access the arrays - row major
+	//---------------------------------------------------------------------------------------------------------
+
+	dev_w[idx_k] = dev_U[idx_u_ik] * dev_V[idx_v_kj];
+	
+	//---------------------------------------------------------------------------------------------------------
+
+}
--- CPP_HMM/CPP_HMM_CUDA/VectorMath.cuh
+++ CPP_HMM/CPP_HMM_CUDA/VectorMath.cuh
@@ -15,28 +15,28 @@
 
 /** Multiplication of row and column vector of two two dimensional matrices.
   * Matrix layout is row mayor, matrix indexing is row first.
-  * @param U first input matrix that provided the row
-  * @param V second input matrix that provides the column
+  * @param host_U first input matrix that provided the row
+  * @param host_V second input matrix that provides the column
   * @param index_row_i row index in the first matrix U
   * @param index_column_j column index in the second matrix V
   * @param dim1_U first dimension of matrix layout in U
   * @param dim1_V first dimension of matrix layout in V
   * @return result of cross product
   */
-__host__ double rowColumnMulMatrixHost(const double *U, const double *V, int index_row_i, int index_column_j, int dim1_U, int dim1_V);
+__host__ double rowColumnMulMatrixHost(const double *host_U, const double *host_V, int index_row_i, int index_column_j, int dim1_U, int dim1_V);
 
 /** Elementwise muliplication of row in the first and column in the second two dimensional matrix.
   * Matrix layout is row mayor, matrix indexing is row first.
-  * @param w vector that holds the result of the elementwise multiplication, needs to be allocated first
-  * @param U first input matrix that provided the row
-  * @param V second input matrix that provides the column
+  * @param host_w vector that holds the result of the elementwise multiplication, needs to be allocated first
+  * @param host_U first input matrix that provided the row
+  * @param host_V second input matrix that provides the column
   * @param index_row_i row index in the first matrix U
   * @param index_column_j column index in the second matrix V
   * @param dim1_U first dimension of matrix layout in U
   * @param dim1_V first dimension of matrix layout in V
   * @return result of cross product
   */
-__host__ void elementMulMatrixHost(double *w, const double *U, const double *V, int index_row_i, int index_column_j, int dim1_U, int dim1_V);
+__host__ void elementMulMatrixHost(double *host_w, const double *host_U, const double *host_V, int index_row_i, int index_column_j, int dim1_U, int dim1_V);
 
 //---------------------------------------------------------------------------------------------------------
 // GPU - parallel implementation
@@ -45,26 +45,29 @@ __host__ void elementMulMatrixHost(double *w, const double *U, const double *V, 
 
 /** Multiplication of row and column vector of two two dimensional matrices.
   * Matrix layout is row mayor, matrix indexing is row first.
-  * @param U first input matrix that provided the row
-  * @param V second input matrix that provides the column
+  * @param dev_U first input matrix that provided the row
+  * @param dev_V second input matrix that provides the column
   * @param index_row_i row index in the first matrix U
   * @param index_column_j column index in the second matrix V
   * @param dim1_U first dimension of matrix layout in U
   * @param dim1_V first dimension of matrix layout in V
   * @return result of cross product
   */
-__device__ double rowColumnMulMatrixDevice(const double *U, const double *V, int index_row_i, int index_column_j, int dim1_U, int dim1_V);
+__device__ double rowColumnMulMatrixDevice(const double *dev_U, const double *dev_V, int index_row_i, int index_column_j, int dim1_U, int dim1_V);
+
+__global__ void rowColumnMulMatrixKernel(double *dev_w, const double *dev_U, const double *dev_V, int index_row_i, int index_column_j, int dim1_U, int dim1_V);
 
 /** Elementwise muliplication of row in the first and column in the second two dimensional matrix.
   * Matrix layout is row mayor, matrix indexing is row first.
-  * @param w vector that holds the result of the elementwise multiplication, needs to be allocated first
-  * @param U first input matrix that provided the row
-  * @param V second input matrix that provides the column
+  * @param dev_w vector that holds the result of the elementwise multiplication, needs to be allocated first
+  * @param dev_U first input matrix that provided the row
+  * @param dev_V second input matrix that provides the column
   * @param index_row_i row index in the first matrix U
   * @param index_column_j column index in the second matrix V
   * @param dim1_U first dimension of matrix layout in U
   * @param dim1_V first dimension of matrix layout in V
   * @return result of cross product
   */
-__device__ void elementMulMatrixDevice(double *w, const double *U, const double *V, int index_row_i, int index_column_j, int dim1_U, int dim1_V);
+__device__ void elementMulMatrixDevice(double *dev_w, const double *dev_U, const double *dev_V, int index_row_i, int index_column_j, int dim1_U, int dim1_V);
 
+__global__ void elementMulMatrixKernel(double *dev_w, const double *dev_U, const double *dev_V, int index_row_i, int index_column_j, int dim1_U, int dim1_V);
