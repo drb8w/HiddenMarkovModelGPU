4f77b9e3781b8093c61280feed4b2865c18570f2
drb8w <drb8w@directbox.com>
2016-06-09 11:37:01 +0200

    * add first implementation

--- CPP_HMM/CPP_HMM_CUDA/VectorMath.cu
+++ CPP_HMM/CPP_HMM_CUDA/VectorMath.cu
@@ -16,8 +16,17 @@ __host__ double rowColumnMulMatrixHost(const double *U, const double *V, int ind
 	//---------------------------------------------------------------------------------------------------------
 	// iterate the arrays
 	//---------------------------------------------------------------------------------------------------------
+	double result = 0;
 
-	return 0;
+	for (int k = 0; k < dim1_U; k++)
+	{
+		int idx_u_ik = idx_u_i0 + k;
+		int idx_v_kj = idx_v_0j + k*dim1_V;
+
+		result += U[idx_u_ik] * V[idx_v_kj];
+	}
+
+	return result;
 }
 
 __host__ void elementMulMatrixHost(double *w, const double *U, const double *V, int index_row_i, int index_column_j, int dim1_U, int dim1_V)
