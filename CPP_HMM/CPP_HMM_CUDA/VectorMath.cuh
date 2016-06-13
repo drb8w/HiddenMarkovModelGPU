3a8b82fada77a683003da08123dd982d219d68e4
Marc <marc.dhaubenstock@gmail.com>
2016-06-13 14:21:23 +0200

    implemented cublas wrapper. x64 only! Needs testing

--- CPP_HMM/CPP_HMM_CUDA/VectorMath.cuh
+++ CPP_HMM/CPP_HMM_CUDA/VectorMath.cuh
@@ -2,7 +2,9 @@
 
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
+#include "Utilities.h"
 
+#include <cublas_v2.h>
 #include <stdio.h>
 #include <cmath>
 #include <fstream>
@@ -91,4 +93,43 @@ __global__ void elementMulMatrixKernel(double *dev_w, const double *dev_U, const
 
 __host__ void calculateAlphaTrellis3DTimeslice(double *dev_Alpha3D, const double *dev_B, const double *dev_A, unsigned int M_noOfObsSequences, unsigned int N_noOfStates, unsigned int T_noOfObservations, unsigned int V_noOfObsSymbols);
 
+//---------------------------------------------------------------------------------------------------------
+
+// EXCERPT TAKEN FROM CUDA SAMPLE: matrixMULCUBLAS
+// CUBLAS library uses column-major storage, but C/C++ use row-major storage.
+// When passing the matrix pointer to CUBLAS, the memory layout alters from
+// row-major to column-major, which is equivalent to an implicit transpose.
+
+// In the case of row-major C/C++ matrix A, B, and a simple matrix multiplication
+// C = A * B, we can't use the input order like cublasSgemm(A, B)  because of
+// implicit transpose. The actual result of cublasSegemm(A, B) is A(T) * B(T).
+// If col(A(T)) != row(B(T)), equal to row(A) != col(B), A(T) and B(T) are not
+// multipliable. Moreover, even if A(T) and B(T) are multipliable, the result C
+// is a column-based cublas matrix, which means C(T) in C/C++, we need extra
+// transpose code to convert it to a row-based C/C++ matrix.
+
+// To solve the problem, let's consider our desired result C, a row-major matrix.
+// In cublas format, it is C(T) actually (because of the implicit transpose).
+// C = A * B, so C(T) = (A * B) (T) = B(T) * A(T). Cublas matrice B(T) and A(T)
+// happen to be C/C++ matrice B and A (still because of the implicit transpose)!
+// We don't need extra transpose code, we only need alter the input order!
+//
+// CUBLAS provides high-performance matrix multiplication.
+// See also:
+// V. Volkov and J. Demmel, "Benchmarking GPUs to tune dense linear algebra,"
+// in Proc. 2008 ACM/IEEE Conf. on Supercomputing (SC '08),
+// Piscataway, NJ: IEEE Press, 2008, pp. Art. 31:1-11.
+//
+
+/** 
+  * Wrapper function for cublas multiplication
+  * @param row_A number of rows in matirx A
+  * @param col_B number of columns in matrix B
+  * @param col_A number of columns in matrix A
+  * @param A matrix A stored on device memory 
+  * @param B matrix B stored on device memory 
+  * @param C result matrix C stored on device memory 
+  */
+
+__host__ void cublasMultiplyDouble(int row_A, int col_B, int col_A,const double* A,const double* B,double* C);
 
