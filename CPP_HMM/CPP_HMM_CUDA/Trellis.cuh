4e47fae8810df6fd73fd00b5a8819ba1aea8a46c
drb8w <drb8w@directbox.com>
2016-06-14 20:34:59 +0200

    * add trellis scaling

--- CPP_HMM/CPP_HMM_CUDA/Trellis.cuh
+++ CPP_HMM/CPP_HMM_CUDA/Trellis.cuh
@@ -9,3 +9,12 @@
 #include <iostream>
 
 __host__ void TrellisInitialization2D(double *host_Alpha_trelis_2D, const double *host_Pi_startProbs_1D, const double *host_B_obsEmissionProbs_2D, const unsigned int *host_O_obsSequence_1D, int T_noOfObservations, int N_noOfStates, int V_noOfObsSymbols);
+
+
+/** Rescales row in trellis identified by time index idx_t to sum of one.
+  * @param host_Alpha_trelis_2D the trellis which row should be rescaled
+  * @param T_noOfObservations number of rows i.e. time entries in the trellis
+  * @param N_noOfStates number of columns i.e. different states in the trellis
+  * @param idx_t time index that identifies the affected row in the trellis
+  */
+__host__ void TrellisScaling2D(double *host_Alpha_trelis_2D, unsigned int T_noOfObservations, unsigned int N_noOfStates, unsigned int idx_t);
